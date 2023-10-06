371158e42782437bb81ea93cf23cc5cf8dcd05b9
denniskimotho <denniskimotho222@gmail.com>
2023-10-06 17:25:56 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
-    const input = document.getElementById('input');
     input.addEventListener('click', function() {
     alert('I was clicked!');
     });
 }
+
+addingEventListener()
